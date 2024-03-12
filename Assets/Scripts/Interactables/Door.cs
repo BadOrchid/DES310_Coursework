fa2f1c85029764344f6ffb49c6e05b38d1b910b4
FirestarW <caiuswalker@gmail.com>
2024-03-12 16:36:17 +0000

    Revert "Doors"
    This reverts commit 482f69577c9efa938134f55b900c876e6793f5af.

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -4,56 +4,27 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    //public Sprite openSprite;
-    //public Sprite closeSprite;
+    public Sprite openSprite;
+    public Sprite closeSprite;
 
     SpriteRenderer spriteRenderer;
 
-    [SerializeField] bool isOpen = false;
     [SerializeField] ObjectivesManager objectivesManager;
 
     void Start() {
         spriteRenderer = GetComponent<SpriteRenderer>();
-
-        // Sets whether closed or open door is active
-        if (isOpen) {
-            transform.GetChild(0).gameObject.SetActive(false);
-            transform.GetChild(1).gameObject.SetActive(true);
-
-        }
-        else {
-            transform.GetChild(0).gameObject.SetActive(true);
-            transform.GetChild(1).gameObject.SetActive(false);
-
-        }
 
     }
 
     // Update is called once per frame
     void Update() {
 
-        // If statements split up for if in future we want to check every x frames and change active door only when isOpen changes
-
         // Checks if Objective is complete
         if (objectivesManager.complete) {
-            isOpen = true;
-
-        } else {
-            isOpen = false;
+            // Do stuff
 
         }
 
-        // Sets whether closed or open door is active
-        if (isOpen) {
-            transform.GetChild(0).gameObject.SetActive(false);
-            transform.GetChild(1).gameObject.SetActive(true);
-
-        } else {
-            transform.GetChild(0).gameObject.SetActive(true);
-            transform.GetChild(1).gameObject.SetActive(false);
-
-        }
 
     }
-
 }
