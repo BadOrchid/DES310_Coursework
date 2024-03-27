9743e1a68e49911f061160a70d1a4298522539cb
FirestarW <caiuswalker@gmail.com>
2024-03-27 14:34:30 +0000

    Door
    Doors now close when both players move into next room

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Door : MonoBehaviour {
     //public Sprite openSprite;
@@ -9,22 +12,27 @@ public class Door : MonoBehaviour {
     SpriteRenderer spriteRenderer;
 
     [SerializeField] bool isOpen = false;
+    [SerializeField] bool playersPastDoor = false;
+    [SerializeField] float facingNextRoomAngle = 300;
     [SerializeField] ObjectivesManager objectivesManager;
 
     SpriteRenderer openRenderer;
     SpriteRenderer closeRenderer;
 
-    Collider2D openCollider;
-    Collider2D closeCollider;
+    EdgeCollider2D doorCollider;
+
+    bool humanPassed = false;
+    bool ghostPased = false;
+
+    Vector2 humanPos1;
+
+    Vector2 ghostPos1;
 
     void Start() {
-        //spriteRenderer = GetComponent<SpriteRenderer>();
-
         closeRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
         openRenderer = transform.GetChild(1).GetComponent<SpriteRenderer>();
 
-        closeCollider = transform.GetChild(0).GetComponent<Collider2D>();
-        //openCollider = transform.GetChild(1).GetComponent<Collider2D>();
+        doorCollider = GetComponent<EdgeCollider2D>();
 
 
         // Sets whether closed or open door is active
@@ -42,45 +50,111 @@ public class Door : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
-        // If statements split up for if in future we want to check every x frames and change active door only when isOpen changes
-
-        // Checks if Objective is complete
-        if (objectivesManager.complete) {
-            isOpen = true;
-
-        }
-        else {
+        // If players are past door stop checking objectives
+        if (humanPassed && ghostPased) {
+            playersPastDoor = true;
             isOpen = false;
 
+
+        } else {
+            playersPastDoor = false;
+
+            // If statements split up for if in future we want to check every x frames and change active door only when isOpen changes
+
+            // Checks if Objective is complete
+            if (objectivesManager.complete) {
+                isOpen = true;
+
+            }
+            else {
+                isOpen = false;
+
+            }
+
         }
 
-        // Sets whether closed or open door is active
-        if (isOpen) {
-            OpenDoor();
+        OpenOrCloseDoor();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.GetComponent<TwoPlayerControls>()) {
+            if (collision.tag == "Human") {
+                humanPos1 = collision.transform.position;
+
+            } else if (collision.tag == "Ghost") {
+                ghostPos1 = collision.transform.position;
+
+            }
 
         }
-        else {
-            CloseDoor();
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.GetComponent<TwoPlayerControls>()) {
+            if (collision.tag == "Human") {
+                float angle = CalcAngle(humanPos1, collision.transform.position);
+                Debug.Log(angle);
+                if (angle < 90 && angle > -90) {
+                    humanPassed = true;
+
+                } else {
+                    humanPassed = false;
+
+                }
+                
+            }
+            else if (collision.tag == "Ghost") {
+                float angle = CalcAngle(ghostPos1, collision.transform.position);
+
+                if (angle < 90 && angle > -90) {
+                    ghostPased = true;
+
+                }
+                else {
+                    ghostPased = false;
+
+                }
+
+            }
 
         }
+
+    }
+
+    float CalcAngle(Vector2 start, Vector2 end) {
+        Vector2 direction = end - start;
+        direction.Normalize();
+        return Vector2.SignedAngle(Quaternion.Euler(0, 0, facingNextRoomAngle) * Vector2.up, direction);
 
     }
 
     void OpenDoor() {
-        closeRenderer.enabled = false;
-        closeCollider.enabled = false;
+        doorCollider.isTrigger = true;
 
+        closeRenderer.enabled = false;
         openRenderer.enabled = true;
-        //openCollider.enabled = true;
 
     }
 
     void CloseDoor() {
-        closeRenderer.enabled = true;
-        closeCollider.enabled = true;
+        doorCollider.isTrigger = false;
 
-        openRenderer.enabled = false;
-        //openCollider.enabled = false;
+        closeRenderer.enabled = true;
+        openRenderer.enabled = false;      
+
+    }
+
+    void OpenOrCloseDoor() {
+        if (isOpen) {
+            OpenDoor();
+
+        } else {
+            CloseDoor();
+
+        }
 
     }
 
