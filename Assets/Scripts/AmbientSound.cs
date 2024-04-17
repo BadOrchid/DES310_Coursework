d355bb7f37e89085d06d74f1179dac1f82846b59
FirestarW <caiuswalker@gmail.com>
2024-04-17 16:56:40 +0100

    Ambient Sounds
    Added sfx for ambient weather.
    
    Currently just loops rain sfx, and thunder / storm sfx plays every 20 seconds
    
    Can be turned off in inspector before play

--- Assets/Scripts/AmbientSound.cs
+++ Assets/Scripts/AmbientSound.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class AmbientSound : MonoBehaviour
 {
 
+    [SerializeField] bool playSound;
     [SerializeField] AudioClip[] sounds;
+    [Range(0, 1)][SerializeField] float[] volumes;
 
     AudioSource audioSource;
 
@@ -13,13 +15,41 @@ public class AmbientSound : MonoBehaviour
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        
+
+        if (playSound) {
+            InvokeRepeating("Storm", 5, 20);
+            InvokeRepeating("Rain", 5, sounds[0].length);
+
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //float deltaTime = Time.deltaTime;
+
+
+    }
+
+    void Rain() {
+        StartCoroutine(PlaySound(0, 0));
+
+    }
+
+    void Storm() {
+
+        StartCoroutine(PlaySound(2, 0));
+        StartCoroutine(PlaySound(1, 2));
+        //StartCoroutine(PlaySound(0, 4));
+
+    }
+
+    IEnumerator PlaySound(int index, float seconds) {
+        yield return new WaitForSeconds(seconds);
+        audioSource.volume = volumes[index];
+        audioSource.PlayOneShot(sounds[index]);
+
     }
 
 
