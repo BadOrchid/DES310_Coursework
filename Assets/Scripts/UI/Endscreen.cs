cd1ff2eb4718a16ef53666ad2d9d94581f58ec6c
BadOrchid <44803008+BadOrchid@users.noreply.github.com>
2024-04-23 02:59:30 +0100

    Imported UI from other branch. Needs testing

---
+++ Assets/Scripts/UI/Endscreen.cs
@@ -0,0 +1,154 @@
+ using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Endscreen : MonoBehaviour
+{
+    public GameObject endScreen;
+
+    public Door finalDoor;
+    [SerializeField] ObjectivesManager manager;
+
+    public bool testBool;
+    bool brainDead = false;
+    bool lolBool = true;
+
+    public GameObject endOne;
+    public GameObject endTwo;
+    public GameObject endThree;
+    public GameObject endFour;
+
+    public float sceneSeconds;
+    public float lmaoSeconds;
+    public float lolSeconds;
+    float baseSceneSeconds;
+    float nextScene = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseSceneSeconds = sceneSeconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lmaoSeconds > 0)
+        {
+            IfItWorks();
+        }
+        EndScreenEnable();
+        if (testBool)
+        {
+            if (nextScene == 0)
+            {
+                EndOne();
+                Timer();
+            }
+            else if (nextScene == 1)
+            {
+                EndTwo();
+                Timer();
+            }
+            else if (nextScene == 2)
+            {
+                EndThree();
+                Timer();
+            }
+            else if (nextScene == 3)
+            {
+                EndFour();
+                Timer();
+            }
+            else
+            {
+                StartScreen();
+            }
+        }
+        DoorKun();
+    }
+
+    public void EndScreenEnable()
+    {
+        if (testBool)
+        {
+            endScreen.SetActive(true);
+        }
+    }
+
+    public void EndOne()
+    {
+        endOne.SetActive(true);
+    }
+
+    public void EndTwo()
+    {
+        endTwo.SetActive(true);
+        endOne.SetActive(false);
+    }
+
+    public void EndThree()
+    {
+        endThree.SetActive(true);
+        endTwo.SetActive(false);
+    }
+
+    public void EndFour()
+    {
+        endFour.SetActive(true);
+        endThree.SetActive(false);
+    }
+
+    void Timer()
+    {
+        sceneSeconds -= Time.unscaledDeltaTime;
+        if (sceneSeconds <= 0)
+        {
+            nextScene += 1;
+            sceneSeconds = baseSceneSeconds;
+        }
+    }
+
+    void DoorKun()
+    {
+        if (brainDead == true)
+        {
+            //if (finalDoor.isOpen)
+            //{
+            //    lolBool = true;
+            //    testBool = true;
+            //}
+        }
+    }
+
+    void StartScreen()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    void IfItWorks()
+    {
+        lmaoSeconds -= Time.unscaledDeltaTime;
+        if (lmaoSeconds <= 0)
+        {
+            brainDead = true;
+            ItWorks();
+        }
+    }
+
+    void ItWorks()
+    {
+        if(lolSeconds >= 0 && lolBool == true)
+        {
+            lolSeconds -= Time.unscaledDeltaTime;
+        }
+        else if (lolSeconds >= 0 && lolBool == false)
+        {
+            lolBool = false;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Failstates.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Failstates : MonoBehaviour
+{
+    public bool humanFail;
+    public bool ghostFail;
+
+    [SerializeField] GameObject overlordPanel;
+    [SerializeField] GameObject imgHumanFail;
+    [SerializeField] GameObject imgGhostFail;
+
+    [SerializeField] Button lastCheckpoint;
+    [SerializeField] Button exit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HumanFailState();
+        GhostFailState();
+    }
+
+    public void HumanFailState()
+    {
+        if (humanFail == true)
+        {
+            overlordPanel.SetActive(true);
+            imgHumanFail.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    public void GhostFailState()
+    {
+        if (ghostFail == true)
+        {
+            overlordPanel.SetActive(true);
+            imgGhostFail.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    public void LastCheckpoint()
+    {
+        humanFail = false;
+        ghostFail = false;
+        overlordPanel.SetActive(false);
+        imgHumanFail.SetActive(false);
+        imgGhostFail.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
